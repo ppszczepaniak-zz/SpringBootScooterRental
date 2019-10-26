2eecb5efa37535137c584b0d2b787016c4fd7163
ppszczepaniak <piotr.p.szczepaniak@gmail.com>
2019-10-26 13:02:56 +0200

    add data.sql

--- src/main/java/com/example/scooterRental/ScooterRentalApplication.java
+++ src/main/java/com/example/scooterRental/ScooterRentalApplication.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.scooterRental.controller"})
+@ComponentScan(basePackages = {
+		"com.example.scooterRental.controller",
+		"com.example.scooterRental.model"
+})
 //in order to properly load components which represent controller we must add this annotation (ComponentScan),
 // which describes which packages should be scanned in order to find Spring components
 
@@ -15,5 +18,4 @@ public class ScooterRentalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScooterRentalApplication.class, args);
 	}
-//TODO add com.example.scooterRentalApp.model for compenent scan?
 }
