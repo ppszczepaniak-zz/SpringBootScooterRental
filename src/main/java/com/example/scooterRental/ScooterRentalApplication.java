b3a253b9bc868f5f44e61bf184ab89359b9b6549
ppszczepaniak <piotr.p.szczepaniak@gmail.com>
2019-10-26 13:35:53 +0200

    add message.properties

--- src/main/java/com/example/scooterRental/ScooterRentalApplication.java
+++ src/main/java/com/example/scooterRental/ScooterRentalApplication.java
@@ -3,20 +3,24 @@ package com.example.scooterRental;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:message.properties", encoding = "UTF-8")
+//we add this so Spring will use our messages
 @ComponentScan(basePackages = {
-		"com.example.scooterRental.controller",
-		"com.example.scooterRental.model",
-		"com.example.scooterRentalApp.repository"
+        "com.example.scooterRental.controller",
+        "com.example.scooterRental.model",
+        "com.example.scooterRentalApp.repository"
 })
-//in order to properly load components which represent controller we must add this annotation (ComponentScan),
+//in order to properly load components which represent controller, model, repositotory,... we must add this annotation (ComponentScan),
 // which describes which packages should be scanned in order to find Spring components
+//INFO: it probably is NOT  necessary if all packages are "below" main class, as in this app
 
 public class ScooterRentalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScooterRentalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScooterRentalApplication.class, args);
+    }
 }
