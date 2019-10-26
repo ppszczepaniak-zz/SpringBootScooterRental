2b874ee0307b9dcbdc30e3ae8bc7ba0b7a7b2c87
ppszczepaniak <piotr.p.szczepaniak@gmail.com>
2019-10-26 10:59:09 +0200

    initial commit

---
+++ src/test/java/com/example/scooterRental/ScooterRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scooterRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScooterRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
