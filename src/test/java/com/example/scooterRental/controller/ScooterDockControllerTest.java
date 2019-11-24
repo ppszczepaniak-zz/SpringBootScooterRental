e58441cdf9a899a2d09862a42a65f0fe7b27a09e
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-11-24 10:52:18 +0100

    first test

--- src/test/java/com/example/scooterRental/controller/ScooterDockControllerTest.java
+++ src/test/java/com/example/scooterRental/controller/ScooterDockControllerTest.java
@@ -8,6 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
 @RunWith(SpringRunner.class) //jUnit 4 adnotation
 @SpringBootTest  //so it uses all Spring functionalities
 @AutoConfigureMockMvc //add to use MockMvc
@@ -18,6 +23,34 @@ public class ScooterDockControllerTest {
 
     @Test
     public void ifGetScootersRequestIsCorrectShouldReturnHttpCode200AndInitialScooterList() throws Exception {
-        mockMvc.perform(null);
+        mockMvc.perform(get("/scooter-dock/{scooterDockId}/scooters", 1))     //1 = placeholder for {scooterDockId}
+                .andExpect(status().is(200))
+                .andExpect(content().json(
+                        "[{\n" +
+                                "\t\"id\": 5,\n" +
+                                "\t\"modelName\": \"ERE-321\",\n" +
+                                "\t\"maxSpeed\": 25,\n" +
+                                "\t\"rentalPrice\": 19.99\n" +
+                                "},\n  " +
+                                "{\n" +
+                                "\t\"id\": 8,\n" +
+                                "\t\"modelName\": \"V-SPEED-2\",\n" +
+                                "\t\"maxSpeed\": 40,\n" +
+                                "\t\"rentalPrice\": 39.99\n" +
+                                "},\n  " +
+                                "{\n" +
+                                "\t\"id\": 6,\n" +
+                                "\t\"modelName\": \"RTT-43z\",\n" +
+                                "\t\"maxSpeed\": 20,\n" +
+                                "\t\"rentalPrice\": 15.5\n" +
+                                "},\n" +
+                                "{\n" +
+                                "\t\"id\": 7,\n" +
+                                "\t\"modelName\": \"V-SPEED-1\",\n" +
+                                "\t\"maxSpeed\": 35,\n" +
+                                "\t\"rentalPrice\": 29.99\n" +
+                                "}]"
+                ));
     }
+
 }
