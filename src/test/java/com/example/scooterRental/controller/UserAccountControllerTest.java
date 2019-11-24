4fc7f850af1d5ec9e261079fe8d8515d1387c49a
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-11-24 13:58:25 +0100

    add userAccountControllerTest

--- src/test/java/com/example/scooterRental/controller/UserAccountControllerTest.java
+++ src/test/java/com/example/scooterRental/controller/UserAccountControllerTest.java
@@ -6,10 +6,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,12 +24,16 @@ public class UserAccountControllerTest {
     @Autowired  //injects MockMvc
     private MockMvc mockMvc;
 
-//    @Test
-//    public void ifGetScootersRequestIsCorrectShouldReturnHttpCode200AndInitialScooterList(int dockId, String escapedJSON) throws Exception {
-//        mockMvc.perform(get("/scooter-dock/{scooterDockId}/scooters", dockId))     //1 = placeholder for {scooterDockId}
-//                .andExpect(status().is(200))
-//                .andExpect(content().json(escapedJSON
-//                ));
-//    }
+    @Test
+    public void ifCreateAccountRequestContainsWrongEmailAddressShouldReturnHttpCode400AndErrorMsg() throws Exception {
+        mockMvc.perform(
+                post("/user-account/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n\t\"ownerAge\": 22,\n\t\"ownerEmail\": \"janekexample.com\",\n\t\"ownerUsername\": \"name\"\n}") //e-mail without "@"
+        )
+                .andExpect(status().is(400))
+                .andExpect(content().json("{\n\t\"errorCode\": \"ERR002\",\n\t\"errorMsg\": \"Podaj poprawny adres e-mail.\",\n\t\"status\": \"ERROR\"\n}"))
+                .andDo(MockMvcResultHandlers.print()); //andDo() allows to add functionality e.g. with print() it will list details of request
+    }
 
 }
