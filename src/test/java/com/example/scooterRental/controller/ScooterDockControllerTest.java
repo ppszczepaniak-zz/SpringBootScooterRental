edd542d902f3c0c4a5ce74d8c8b1a53aec1fbfd2
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-11-24 10:12:10 +0100

    integration tests initial commit

---
+++ src/test/java/com/example/scooterRental/controller/ScooterDockControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.scooterRental.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) //jUnit 4 adnotation
+@SpringBootTest
+public class ScooterDockControllerTest {
+
+    @Test
+    public void ifGetScootersRequestIsCorrectShouldReturnHttpCode200AndInitialScooterList(){
+
+    }
+}
