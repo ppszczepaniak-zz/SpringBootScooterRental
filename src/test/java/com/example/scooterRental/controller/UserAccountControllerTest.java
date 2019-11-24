8d9605f960598bf439ac8e987753c05201092b96
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-11-24 16:55:44 +0100

    all tests working

--- src/test/java/com/example/scooterRental/controller/UserAccountControllerTest.java
+++ src/test/java/com/example/scooterRental/controller/UserAccountControllerTest.java
@@ -19,7 +19,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Optional;
 
