dca79a4ea7973b1ba89eddf9669db9228f322416
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-11-24 10:26:39 +0100

    integration tests in progress

--- src/test/java/com/example/scooterRental/controller/ScooterDockControllerTest.java
+++ src/test/java/com/example/scooterRental/controller/ScooterDockControllerTest.java
@@ -2,15 +2,22 @@ package com.example.scooterRental.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class) //jUnit 4 adnotation
-@SpringBootTest
+@SpringBootTest  //so it uses all Spring functionalities
+@AutoConfigureMockMvc //add to use MockMvc
 public class ScooterDockControllerTest {
 
-    @Test
-    public void ifGetScootersRequestIsCorrectShouldReturnHttpCode200AndInitialScooterList(){
+    @Autowired  //injects MockMvc
+    private MockMvc mockMvc;
 
+    @Test
+    public void ifGetScootersRequestIsCorrectShouldReturnHttpCode200AndInitialScooterList() throws Exception {
+        mockMvc.perform(null);
     }
 }
